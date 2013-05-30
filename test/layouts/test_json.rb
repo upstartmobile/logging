501309550fd03ed635b132b5ecfb82d189b3f2bc
Alex Bisceglie <alex@cabulous.com>
2013-05-30 14:14:52 -0700

    [#50201635] support hostname directive in layouts

--- test/layouts/test_json.rb
+++ test/layouts/test_json.rb
@@ -4,55 +4,45 @@ require File.expand_path('../setup', File.dirname(__FILE__))
 module TestLogging
 module TestLayouts
 
-  class TestJson < Test::Unit::TestCase
+  class TestYaml < Test::Unit::TestCase
     include LoggingTestCase
 
     def setup
       super
-      @layout = Logging.layouts.json({})
+      @layout = Logging.layouts.yaml({})
       @levels = Logging::LEVELS
       @date_fmt = '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}(Z|[+-]\d{2}:\d{2})'
       Thread.current[:name] = nil
     end
 
-    def test_initializer
-      assert_raise(ArgumentError) {
-        Logging.layouts.parseable.new :style => :foo
-      }
-    end
-
     def test_format
+      h = {
+        'level' => 'INFO',
+        'logger' => 'ArrayLogger',
+        'message' => 'log message'
+      }
+
       event = Logging::LogEvent.new('ArrayLogger', @levels['info'],
                                     'log message', false)
-      format = @layout.format(event)
-      assert_match %r/"timestamp":"#@date_fmt"/, format
-      assert_match %r/"level":"INFO"/, format
-      assert_match %r/"logger":"ArrayLogger"/, format
-      assert_match %r/"message":"log message"/, format
+      assert_yaml_match h, @layout.format(event)
 
       event.data = [1, 2, 3, 4]
-      format = @layout.format(event)
-      assert_match %r/"timestamp":"#@date_fmt"/, format
-      assert_match %r/"level":"INFO"/, format
-      assert_match %r/"logger":"ArrayLogger"/, format
-      assert_match %r/"message":\[1,2,3,4\]/, format
+      h['message'] = [1,2,3,4]
+      assert_yaml_match h, @layout.format(event)
 
       event.level = @levels['debug']
       event.data = 'and another message'
-      format = @layout.format(event)
-      assert_match %r/"timestamp":"#@date_fmt"/, format
-      assert_match %r/"level":"DEBUG"/, format
-      assert_match %r/"logger":"ArrayLogger"/, format
-      assert_match %r/"message":"and another message"/, format
+      h['level'] = 'DEBUG'
+      h['message'] = 'and another message'
+      assert_yaml_match h, @layout.format(event)
 
       event.logger = 'Test'
       event.level = @levels['fatal']
       event.data = Exception.new
-      format = @layout.format(event)
-      assert_match %r/"timestamp":"#@date_fmt"/, format
-      assert_match %r/"level":"FATAL"/, format
-      assert_match %r/"logger":"Test"/, format
-      assert_match %r/"message":\{(?:"(?:class|message)":"Exception",?){2}\}/, format
+      h['level'] = 'FATAL'
+      h['logger'] = 'Test'
+      h['message'] = {:class => 'Exception', :message => 'Exception'}
+      assert_yaml_match h, @layout.format(event)
     end
 
     def test_items
@@ -65,7 +55,7 @@ module TestLayouts
 
       @layout.items = %w[timestamp]
       assert_equal %w[timestamp], @layout.items
-      assert_match %r/\{"timestamp":"#@date_fmt"\}\n/, @layout.format(event)
+      assert_match %r/\A--- ?\ntimestamp: ["']#@date_fmt["']\n/, @layout.format(event)
 
       # 'foo' is not a recognized item
       assert_raise(ArgumentError) {
@@ -81,42 +71,42 @@ module TestLayouts
       event.method = 'method_name'
 
       @layout.items = %w[logger]
-      assert_equal %Q[{"logger":"TestLogger"}\n], @layout.format(event)
+      assert_match %r/\A--- ?\nlogger: TestLogger\n/, @layout.format(event)
 
       @layout.items = %w[file]
-      assert_equal %Q[{"file":"test_file.rb"}\n], @layout.format(event)
+      assert_match %r/\A--- ?\nfile: test_file.rb\n/, @layout.format(event)
 
       @layout.items = %w[level]
-      assert_equal %Q[{"level":"INFO"}\n], @layout.format(event)
+      assert_match %r/\A--- ?\nlevel: INFO\n/, @layout.format(event)
 
       @layout.items = %w[line]
-      assert_equal %Q[{"line":123}\n], @layout.format(event)
+      assert_match %r/\A--- ?\nline: 123\n/, @layout.format(event)
 
       @layout.items = %w[message]
-      assert_equal %Q[{"message":"log message"}\n], @layout.format(event)
+      assert_match %r/\A--- ?\nmessage: log message\n/, @layout.format(event)
 
       @layout.items = %w[method]
-      assert_equal %Q[{"method":"method_name"}\n], @layout.format(event)
+      assert_match %r/\A--- ?\nmethod: method_name\n/, @layout.format(event)
 
       @layout.items = %w[pid]
-      assert_match %r/\A\{"pid":\d+\}\n\z/, @layout.format(event)
+      assert_match %r/\A--- ?\npid: \d+\n\z/, @layout.format(event)
 
       @layout.items = %w[millis]
-      assert_match %r/\A\{"millis":\d+\}\n\z/, @layout.format(event)
+      assert_match %r/\A--- ?\nmillis: \d+\n\z/, @layout.format(event)
 
       @layout.items = %w[thread_id]
-      assert_match %r/\A\{"thread_id":-?\d+\}\n\z/, @layout.format(event)
+      assert_match %r/\A--- ?\nthread_id: -?\d+\n\z/, @layout.format(event)
 
       @layout.items = %w[thread]
-      assert_equal %Q[{"thread":null}\n], @layout.format(event)
+      assert_match %r/\A--- ?\nthread: ?\n/, @layout.format(event)
       Thread.current[:name] = "Main"
-      assert_equal %Q[{"thread":"Main"}\n], @layout.format(event)
+      assert_match %r/\A--- ?\nthread: Main\n/, @layout.format(event)
 
       @layout.items = %w[mdc]
-      assert_match %r/\A\{"mdc":\{\}\}\n\z/, @layout.format(event)
+      assert_match %r/\A--- ?\nmdc: \{\}\n/, @layout.format(event)
 
       @layout.items = %w[ndc]
-      assert_match %r/\A\{"ndc":\[\]\}\n\z/, @layout.format(event)
+      assert_match %r/\A--- ?\nndc: \[\]\n/, @layout.format(event)
     end
 
     def test_mdc_output
@@ -128,15 +118,11 @@ module TestLayouts
       @layout.items = %w[timestamp level logger message mdc]
 
       format = @layout.format(event)
-      assert_match %r/"timestamp":"#@date_fmt"/, format
-      assert_match %r/"level":"INFO"/, format
-      assert_match %r/"logger":"TestLogger"/, format
-      assert_match %r/"message":"log message"/, format
-      assert_match %r/"mdc":\{(?:(?:"X-Session":"123abc"|"Cookie":"monster"),?){2}\}/, format
+      assert_match %r/\nmdc: ?(?:\n  (?:X-Session: 123abc|Cookie: monster)\n?){2}/, format
 
       Logging.mdc.delete 'Cookie'
       format = @layout.format(event)
-      assert_match %r/"mdc":\{"X-Session":"123abc"\}/, format
+      assert_match %r/\nmdc: ?\n  X-Session: 123abc\n/, format
     end
 
     def test_ndc_output
@@ -148,22 +134,30 @@ module TestLayouts
       @layout.items = %w[timestamp level logger message ndc]
 
       format = @layout.format(event)
-      assert_match %r/"timestamp":"#@date_fmt"/, format
-      assert_match %r/"level":"INFO"/, format
-      assert_match %r/"logger":"TestLogger"/, format
-      assert_match %r/"message":"log message"/, format
-      assert_match %r/"ndc":\["context a","context b"\]/, format
+      assert_match %r/\nndc: ?\n\s*- context a\n\s*- context b\n/, format
 
       Logging.ndc.pop
       format = @layout.format(event)
-      assert_match %r/"ndc":\["context a"\]/, format
+      assert_match %r/\nndc: ?\n\s*- context a\n/, format
 
       Logging.ndc.pop
       format = @layout.format(event)
-      assert_match %r/"ndc":\[\]/, format
+      assert_match %r/\nndc: \[\]\n/, format
     end
 
-  end  # class TestJson
+  private
+
+    def assert_yaml_match( expected, actual )
+      actual = YAML.load(actual)
+
+      assert_instance_of String, actual['timestamp']
+      assert_instance_of Time, Time.parse(actual['timestamp'])
+      assert_equal expected['level'], actual['level']
+      assert_equal expected['logger'], actual['logger']
+      assert_equal expected['message'], actual['message']
+    end
+
+  end  # class TestYaml
 end  # module TestLayouts
 end  # module TestLogging
 
